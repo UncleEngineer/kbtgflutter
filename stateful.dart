3020a4cb65255a13dd96171777260f086dd2863c
Uncle Engineer <loong.wissawakorn@gmail.com>
2020-05-13 15:19:49 +0700

    Stateful Samples

---
+++ stateful.dart
@@ -0,0 +1,13 @@
+class XXXXX extends StatefulWidget {
+  @override
+  _XXXXXState createState() => _XXXXXState();
+}
+
+class _XXXXXState extends State<XXXXX> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
